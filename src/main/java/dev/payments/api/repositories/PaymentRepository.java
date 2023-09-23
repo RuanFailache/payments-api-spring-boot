53b17eb2754c2e4347ee91394b1cc393d08a0430
Ruan Failache <ruanfailache@gmail.com>
2023-09-23 14:38:13 -0300

    refactor(payments): remove unused method at repository

--- src/main/java/dev/payments/api/repositories/PaymentRepository.java
+++ src/main/java/dev/payments/api/repositories/PaymentRepository.java
@@ -1,18 +1,9 @@
 package dev.payments.api.repositories;
 
 import dev.payments.api.entities.Payment;
-import dev.payments.api.entities.PaymentStatus;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
-    Page<Payment> findAllByDebitCodeOrUserIdentificationOrStatus(
-            Long debitCode,
-            String userIdentification,
-            PaymentStatus status,
-            Pageable pageable
-    );
 }
